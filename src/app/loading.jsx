5241a0376a3397b4746bdfa022a3ebf20b4cd820
Kumar Ayush <140546983+Kumar-AyushD@users.noreply.github.com>
2024-03-12 01:44:46 +0530

    g

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      <Image className="h-52" width={10} src="spinner.svg" alt="loading..." />
+      <Image className="h-52" width={10} src="spinner.svg" alt="loading..." height={10} />
     </div>
   );
 }
